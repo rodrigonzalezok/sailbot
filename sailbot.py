63f997ac0c7ba3b83a015d5117de77340691ae88
Rodrigo Gonzalez <69409908+rodrigonzalezok@users.noreply.github.com>
2022-01-19 20:58:19 -0300

    Add files via upload

---
+++ sailbot.py
@@ -0,0 +1,247 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.relative_locator import locate_with
+from selenium.webdriver.common.keys import Keys
+import time
+import json
+from os import system
+from time import sleep
+import random
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#---------------------------------------- NAVEGADOR WEB DONDE ESTARÁ CORRIENDO LA PÁGINA DONDE TRABAJAREMOS ---------------------------------------------------------
+
+# Creamos la instancia del navegador
+def browser_init():
+    # Creamos las sesiones
+    global sesion_1, sesion_2, sesion_3, sesion_4
+    sesion_1 = webdriver.Chrome(executable_path=".\driver\chromedriver.exe")
+    sesion_2 = webdriver.Chrome(executable_path=".\driver\chromedriver.exe")
+    sesion_3 = webdriver.Chrome(executable_path=".\driver\chromedriver.exe")
+    sesion_4 = webdriver.Chrome(executable_path=".\driver\chromedriver.exe")
+    # Abrimos las páginas y maximizamos las ventanas
+    sesion_1.get('https://web.whatsapp.com/')
+    sesion_2.get('https://web.whatsapp.com/')
+    sesion_3.get('https://web.whatsapp.com/')
+    sesion_4.get('https://web.whatsapp.com/')
+    sesion_1.maximize_window()
+    sesion_2.maximize_window()
+    sesion_3.maximize_window()
+    sesion_4.maximize_window()
+    # Qr
+    print("Por favor, escanea los códigos QR")
+    # Esperamos a que se cargue la pagina
+    WebDriverWait(sesion_1, 30000).until(expected_conditions.visibility_of_element_located((By.ID, 'side')))
+    WebDriverWait(sesion_2, 30000).until(expected_conditions.visibility_of_element_located((By.ID, 'side')))
+    WebDriverWait(sesion_3, 30000).until(expected_conditions.visibility_of_element_located((By.ID, 'side')))
+    WebDriverWait(sesion_4, 30000).until(expected_conditions.visibility_of_element_located((By.ID, 'side')))
+    print("Whatsapp iniciado")
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#---------------------------------------- CÓDIGO QUE RECONOCE LOS MENSAJES Y OFRECE RESPUESTAS ACORDE A LAS MISMAS ---------------------------------------------------------
+
+# Creamos variable de sesiones
+global numero_sesion
+numero_sesion = 1
+
+# Mensajes a enviar
+def send_message(msj, n_sesion):
+
+    # Condicionamos la sesión desde la que se enviará el mensaje
+    if n_sesion == 1:
+        browser = sesion_1
+    elif n_sesion == 2:
+        browser = sesion_2
+    elif n_sesion == 3:
+        browser = sesion_3
+    elif n_sesion == 4:
+        browser = sesion_4
+
+    # Utilizamos el diccionario json para elegir respuestas
+    with open('./diccionario.json', encoding = "utf-8") as diccionario_mensajes:
+        diccionario = json.load(diccionario_mensajes)
+        
+    # Hacemos click en el mensaje nuevo
+    nombre_contacto = browser.find_element(locate_with(By.TAG_NAME, 'div').above(msj))
+    nombre_contacto.click()
+    time.sleep(0.5)
+
+    # Si aparece un mensaje de un destinatario nuevo, damos click en 'Ok'
+    try:
+        if browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[2]/div/div[1]/div[1]/div/div[2]/div[3]/div/div/div').text == "OK":
+            browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[2]/div/div[1]/div[1]/div/div[2]/div[3]/div/div/div').click()
+    except:
+        pass
+    
+################ Codigo de emoji  :emoji' u'1F916' 
+
+    # Creamos variables
+    mensaje_recibido = browser.find_elements(By.CSS_SELECTOR, 'div._22Msk div div._1Gy50')[-1].text
+    mensaje_enviado = ""
+    escribir_mensaje = browser.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')
+    if len(browser.find_elements(By.CSS_SELECTOR, 'div._22Msk div div._1Gy50')) >= 2:
+        mensaje_enviado = browser.find_elements(By.CSS_SELECTOR, 'div._22Msk div div._1Gy50')[-2].text
+    mensaje_enviado_imagen = browser.find_elements(By.CSS_SELECTOR, 'div._22Msk div div._1Gy50')[-1].text
+    
+    # Verificamos qué mensaje nos llegó
+
+    # Mensaje ABRIR CUENTA
+    if mensaje_recibido.title() in diccionario.get('Abrir cuenta'):
+        time.sleep(1)
+        # Enviamos un mensaje
+        try:
+            escribir_mensaje.send_keys(diccionario.get('Respuesta a Abrir cuenta') + Keys.ENTER + Keys.ENTER)            
+        except:
+            print("No se pudo enviar el mensaje")
+
+    """
+    # Mensaje RECIBE NOMBRE DE USUARIO Y PIDE EMAIL
+    if mensaje_enviado == diccionario.get('Respuesta a Abrir cuenta'):
+        time.sleep(1)
+        # Almacenamos el nombre del contacto que nos envia
+        nombre_cliente = mensaje_recibido.title()
+        # Enviamos un mensaje
+        try:
+            escribir_mensaje.send_keys("Muchas gracias *" + nombre_cliente + diccionario.get('Respuesta a Nombre') + Keys.ENTER)
+        except:
+            print("No se pudo enviar el mensaje")
+
+    # Mensaje RECIBE EMAIL DE USUARIO Y PREGUNTA SI LA CUENTA ES PARA TITULAR O CON COTITULAR
+    if mensaje_enviado == "Para comenzar te voy a pedir que me envies el email con el que querés abrir tu cuenta en Sailing Inversiones, dentro de poco vas a recibir la confirmación de la apertura en ese correo.":
+        time.sleep(1)
+        # Enviamos un mensaje
+        try:
+            escribir_mensaje.send_keys(diccionario.get('Respuesta a Email') + Keys.ENTER)
+        except:
+            print("No se pudo enviar el mensaje")
+
+    # Mensaje ELIJE TITULAR Y RESPONDE PIDIENDO FOTO FRENTE DNI
+    if mensaje_enviado == "Responde con la letra correspondiente a la opción que desees" and mensaje_recibido.title() in diccionario.get('Titular'):
+        time.sleep(1)
+        # Enviamos un mensaje
+        try:
+            escribir_mensaje.send_keys(diccionario.get('Respuesta a Titular') + Keys.ENTER)
+        except:
+            print("No se pudo enviar el mensaje") 
+
+    # Mensaje RECIBE FOTO FRENTE DNI Y RESPONDE PIDIENDO FOTO DORSO DNI
+    try:
+        if browser.find_element(By.CSS_SELECTOR, 'div div._1bJJV div._3IfUe img').get_attribute('class') == 'jciay5ix tvf2evcx oq44ahr5 lb5m6g5c' and mensaje_enviado_imagen == "Por favor, verifica que se vean las 4 esquinas del DNI y que la imagen se vea lo más nítida posible.":
+            time.sleep(1)
+            # Enviamos un mensaje
+            try:
+                escribir_mensaje.send_keys(diccionario.get('Respuesta a Frente DNI') + Keys.ENTER)
+            except:
+                print("No se pudo enviar el mensaje") 
+    except:
+        pass
+
+    # Mensaje RECIBE FOTO DORSO DNI Y RESPONDE PIDIENDO SELFIE CON DNI
+    try:
+        if browser.find_element(By.XPATH, '//*[@id="main"]/div[3]/div/div[2]/div[3]/div[17]/div/div[1]/div/div/div[1]/div[1]/div[2]/img').get_attribute('class') == "jciay5ix tvf2evcx oq44ahr5 lb5m6g5c" and mensaje_enviado_imagen == "¡Excelente! Ahora te voy a pedir que me mandes una foto del DORSO de tu DNI. Por favor, verifica que se vean las 4 esquinas del DNI y que la foto sea lo más nítida posible.":
+            time.sleep(1)
+            # Enviamos un mensaje
+            try:
+                escribir_mensaje.send_keys(diccionario.get('Respuesta a Dorso DNI') + Keys.ENTER)
+            except:
+                print("No se pudo enviar el mensaje")
+    except:
+        pass
+
+    # Mensaje RECIBE SELFIE CON DNI Y RESPONDE PIDIENDO SELFIE CON GESTO
+    try:
+        if browser.find_element(By.XPATH, '//*[@id="main"]/div[3]/div/div[2]/div[3]/div[17]/div/div[1]/div/div/div[1]/div[1]/div[2]/img').get_attribute('class') == "jciay5ix tvf2evcx oq44ahr5 lb5m6g5c" and mensaje_enviado_imagen == "¡Sonríe!":
+            time.sleep(1)
+            # Enviamos un mensaje
+            try:
+                escribir_mensaje.send_keys(random.choice(diccionario.get('Respuesta a Selfie DNI')) + Keys.ENTER)
+            except:
+                print("No se pudo enviar el mensaje")
+    except:
+        pass
+
+    # Mensaje RECIBE SELFIE CON GESTO Y RESPONDE PIDIENDO ESTADO CIVIL
+    try:
+        if browser.find_element(By.XPATH, '//*[@id="main"]/div[3]/div/div[2]/div[3]/div[17]/div/div[1]/div/div/div[1]/div[1]/div[2]/img').get_attribute('class') == "jciay5ix tvf2evcx oq44ahr5 lb5m6g5c" and mensaje_enviado_imagen == "¡WHISHY!":
+            time.sleep(1)
+            # Enviamos un mensaje
+            try:
+                escribir_mensaje.send_keys(diccionario.get('Respuesta a Selfie Aleatoria') + Keys.ENTER)
+            except:
+                print("No se pudo enviar el mensaje")
+    except:
+        pass
+
+    # Mensaje RECIBE ESTADO CIVIL Y RESPONDE PIDIENDO RELACIÓN DE DEPENDENCIA
+    if mensaje_enviado == "d. Viudo/a" and mensaje_recibido.title() in diccionario.get('a', 'b', 'c', 'd'):
+        time.sleep(1)
+        # Enviamos un mensaje
+        try:
+            escribir_mensaje.send_keys(diccionario.get('Respuesta a Estado Civil') + Keys.ENTER)
+        except:
+            print("No se pudo enviar el mensaje")
+
+    # Mensaje RECIBE RELACIÓN DE DEPENDENCIA Y RESPONDE PIDIENDO ALCANCE EN ESTADOS OBLIGADOS
+    if mensaje_enviado == "e. Estudiante" and mensaje_recibido.title() in diccionario.get('a', 'b', 'c', 'd', 'e'):
+        time.sleep(1)
+        # Enviamos un mensaje
+        try:
+            escribir_mensaje.send_keys(diccionario.get('Respuesta a Relacion de Dependencia') + Keys.ENTER)
+        except:
+            print("No se pudo enviar el mensaje")
+
+    # Mensaje FINALIZACIÓN
+    if mensaje_enviado == "g. Ninguno" and mensaje_recibido.title() in diccionario.get('a', 'b', 'c', 'd', 'e', 'f', 'g'):
+        time.sleep(1)
+        # Enviamos un mensaje
+        try:
+            escribir_mensaje.send_keys(diccionario.get('Respuesta a Relacion de Dependencia') + Keys.ENTER)
+        except:
+            print("No se pudo enviar el mensaje")
+
+    # Mensaje ELIGE 2 O MÁS TITULARES Y RESPONDE PIDIENDO FOTO FRENTE DNI
+    if mensaje_enviado == "Responde con la letra correspondiente a la opción que desees." and mensaje_recibido.title() in diccionario.get('Cotitular'):
+        time.sleep(1)
+        # Enviamos un mensaje
+        try:
+            escribir_mensaje.send_keys(diccionario.get('Respuesta a Cotitular') + Keys.ENTER)
+        except:
+            print("No se pudo enviar el mensaje")
+"""
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#---------------------------------------- CÓDIGO QUE VERIFICA QUE HAYA O NO NUEVOS MENSAJES Y EJECUTA LA FUNCIÓN SEND_MESSAGE() ---------------------------------------------------------
+
+# Verificamos que haya un mensaje nuevo
+def check_new_message(): 
+    while True:
+        system("cls")
+        # Almacenamos los mensajes nuevos
+        caja_mensajes = sesion_1.find_elements(By.CSS_SELECTOR, 'div._1pJ9J span._23LrM')
+        contador = 0
+        # Si hay mensajes nuevos
+        if len(caja_mensajes) > 0:
+            # Recorremos todas las cajas de mensajes
+            for mensaje in caja_mensajes:
+                global numero_sesion
+                send_message(mensaje, numero_sesion)
+                numero_sesion += 1
+                if numero_sesion > 4:
+                    numero_sesion = 1
+                contador += 1
+                time.sleep(1)
+                print("Quedan",contador,"mensajes nuevos")
+        else:
+            print("No hay mensajes nuevos")
+        time.sleep(3)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------ INICIA EL SCRIPT -------------------------------------------------------------------------------------------
+
+browser_init()
+check_new_message()
+time.sleep(100000)
